fea6d31c7059535e070f474d98d7a40d02602d98
Wentao Ouyang <oywtece@yahoo.com>
2020-08-07 11:44:22 +0800

    Update

--- data/run_tfrecord_writer.sh
+++ data/run_tfrecord_writer.sh
@@ -2,10 +2,9 @@
 name_1=Movies_Books
 name_2=Books
 
-cat ${name_1}_train.csv | python tfrecord_writer.py ${name_1}_train 25
-cat ${name_1}_val.csv | python tfrecord_writer.py ${name_1}_val 25
-cat ${name_1}_test.csv | python tfrecord_writer.py ${name_1}_test 25
-cat ${name_2}_train.csv | python tfrecord_writer.py ${name_2}_train 715
-cat ${name_2}_val.csv | python tfrecord_writer.py ${name_2}_val 715
-cat ${name_2}_test.csv | python tfrecord_writer.py ${name_2}_test 715
-
+cat ${name_1}_train.csv | python tfrecord_writer.py ${name_1}_train
+cat ${name_1}_val.csv | python tfrecord_writer.py ${name_1}_val
+cat ${name_1}_test.csv | python tfrecord_writer.py ${name_1}_test
+cat ${name_2}_train.csv | python tfrecord_writer.py ${name_2}_train
+cat ${name_2}_val.csv | python tfrecord_writer.py ${name_2}_val
+cat ${name_2}_test.csv | python tfrecord_writer.py ${name_2}_test
